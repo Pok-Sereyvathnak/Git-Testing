e98c6a7db2165108e8d4d04c6e6e6b87172382d9
pok sereyvathnak <poksereyvathnak17@gmail.com>
2026-09-16 15:39:35 +0700

    Add Testing index.php to git Repository

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-"" 
+<?php 
+declare(strict_types= 1);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Testing</title>
+</head>
+<body>
+    <H1>1st Git Testing Text and PHP include</H1>
+    <?php 
+    $Name = "Pok Sereyvathnak";
+    $Age = "N/A";
+    echo "". $Name ."". $Age ."";
+    echo "<br>";
+    echo "Welcome to Git after fetch and Push";
+    echo "<br>";
+    
+    
+    ?>
+
+</body>
+</html>
